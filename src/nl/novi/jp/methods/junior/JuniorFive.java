68d4364c75f6f03d019a00a7e781385e6f8f4fbd
jenjohmar <johannajennifer4@gmail.com>
2022-01-07 14:24:19 +0100

    one

---
+++ src/nl/novi/jp/methods/junior/JuniorFive.java
@@ -0,0 +1,24 @@
+package nl.novi.jp.methods.junior;
+
+import java.util.Locale;
+
+/**
+ * Deze klasse is helemaal leeg, je mag nu dus alles zelf doen.
+ * Maak een methode die een String ontvangt en deze in HOOFDLETTERS uitprint.
+ * Bedenk zelf een goede Engelse naam voor de methode.
+ *
+ * Hoe maak je hoofdletters van een String in Java?
+ * https://duckduckgo.com/?q=java+to+uppercase
+ */
+
+public class JuniorFive {
+
+    public static void main(String[] args) {
+        printCapitals("Goedemiddag saampjes");
+    }
+
+    public static void printCapitals(String string) {
+
+        System.out.println(string.toUpperCase(Locale.ROOT));
+    }
+}
---
+++ src/nl/novi/jp/methods/junior/JuniorTwo.java
@@ -0,0 +1,27 @@
+package nl.novi.jp.methods.junior;
+
+
+/**
+ * Deze klasse bevat al de methode met de naam doubleIt. Deze methode ontvangt een integer, verdubbelt deze en print
+ * deze vervolgens uit.
+ *
+ * Maak deze klasse af door de methode squareTheNumber af te maken. Deze methode ontvangt een integer en moet deze
+ * kwadrateren en uitprinten.
+ */
+public class JuniorTwo {
+
+    public static void main(String[] args) {
+        doubleIt(33);
+        squareTheNumber(23);
+    }
+
+    public static void doubleIt(int number) {
+        int doubledNumber = number + number;
+        System.out.println("Het getal " + number + " is verdubbeld: " + doubledNumber);
+    }
+
+    public static void squareTheNumber(int number) {
+        int squaredNumber = number * number;
+        System.out.println("Het getal " + number + " is gekwadrateerd: " + squaredNumber);
+    }
+}
